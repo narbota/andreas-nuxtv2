94af0baafbce4367856a9eac7b6c7aa498620cab
Andrea N <andrea@narbot.com>
2023-07-05 18:11:29 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Index Page</h1>
+    <h2>I'm Andrea! </h2>
+    <h3> I'm a Full Stack Developer</h3>
+    <img src="https://images.unsplash.com/photo-1688367785331-053647432ce2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60" alt="my image">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+// server side rendering
+
+// Path: pages/about.vue
